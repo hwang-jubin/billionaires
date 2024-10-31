0d5d4507479780735f16d5b9e6c789ddab7e4f0f
황주빈 <hjb06288@gmail.com>
2024-10-31 19:00:46 +0900

    사진 불러오기

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // images: {
+  //   domains: ["https://specials-images.forbesimg.com"], // 추가할 도메인
+  // },
 };
 
 export default nextConfig;
